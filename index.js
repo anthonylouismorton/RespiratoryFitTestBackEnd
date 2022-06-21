4af5307ac212b248d7559d7cb8a8e3902bd36113
anthonymorton <anthonymorton760@gmail.com>
2022-06-20 20:05:09 -0700

    was able to successfully connect to azure server and query the company table

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 'use strict'
-var sql = require('mssql')
 const app = require('./lib/server.js')
 require('dotenv').config();
 const PORT = process.env.PORT || 3055;
-var dbConfig = {
-    server: process.env.SERVER,
-    database: "",
-    user: process.env.USER,
-    password:process.env.PASSWORD,
-    options: {
-        encrypt: true
-    }
-
-}
 app.start(PORT)
